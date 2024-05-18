03ad0c29b418f71883186a7016cbb1e224615056
Crldry <162985335+Crldry@users.noreply.github.com>
2024-05-19 01:23:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,239 @@
+//poem0
+let poem0 = "Ode to Drey: The Web Weaver";
+
+let realm = "realm";
+let pixels = "pixels";
+let code = "code";
+let web_weaver = "web weaver";
+let pearls = "pearls";
+
+let sentence1 = "In the " + realm + " of ones and zeros,";
+let sentence2 = "Where " + pixels + " dance and " + code + "unfurls";
+let sentence3 = "There resides a silent sorcerer,";
+let sentence4 = 'A ' + web_weaver + " with dreams and " + pearls;
+
+
+
+
+//poem1
+let poem1 = "The Blank Canvas";
+
+let rod = "Drey";
+let alchemist_of_the_browser = "alchemist of the browser";
+let keyboard = "keyboard";
+let wand = "wand";
+let pages = "pages";
+let painting = "Painting";
+
+let sentence5 = rod + ", " + "the " + alchemist_of_the_browser + ",";
+let sentence6 = "Wields his " + keyboard + " like a " + wand;
+let sentence7 = "He conjures " + pages + " from thin air, ";
+let sentence8 = painting + " landscape with lines so fond.";
+
+
+//poem2
+let poem2 = "The HTML Symphony";
+
+let fingers = "fingers";
+let keys = "keys";
+let tags = "tags";
+let html = "HTML"
+let symphonies = "symphonies";
+
+let sentence9 = "His " + fingers + "dance upon the " + keys ;
+let sentence10 = "Crafting " + tags + " like musical notes";
+let sentence11 = html + " whispers secrets to the breeze,";
+let sentence12 = "As" + rod + " orchestrates " + symphonies + " in quotes.";
+
+//poem 3
+let poem3 = "The CSS Enchanter";
+
+let css_cloak = "CSS cloak";
+let styling = "Styling";
+let colors = "Colors";
+let bloom = "bloom";
+let font = "font";
+let sing = "sing"
+let evoke = "evoke"
+let visual = "visual";
+
+let sentence13 = "Next, he dons his " + css_cloak + ', ' ;
+let sentence14 = styling + " elements with flair and grace, ";
+let sentence15 = colors + bloom + ', ' + font + sing + ', ' + "layouts" + evoke;
+let sentence16 = "A " + visual + "ballet in this digital space.";
+
+//poem4
+let poem4 = "The Javascript Magician";
+
+let magic = "magic";
+let javascript_spells = "JavaScript spells";
+let will = "will";
+let forms = "Forms";
+let AP = "animations propel";
+
+let sentence17 = "But wait, the " + magic + "deepens still,";
+let sentence18 = rod + " summons, " + javascript_spells ;
+let sentence19 = "Interactivity blooms at his " + will ;
+let sentence20 = forms + " validate, " + AP;
+
+
+//poem5
+let poem5 = "The Debugging Sage";
+
+let bugs = "bugs";
+let debugging = "debugging spectacles";
+let glitches = "glitches";
+let codes_arc = "code's arc";
+let tames = "tames";
+let variables = "variables";
+
+let sentence21 = "Yet, " + bugs + " creep in like shadows dark, ";
+let sentence22 = rod + "dons his " + debugging + ',';
+let sentence23 = "Chasing " + glitches + "through the " +codes_arc + ', ';
+let sentence24 = "He " + tames + "wild loops and elusive " + variables;
+
+//poem6
+let poem6 = "The Responsive Voyager";
+let across = "across";
+let devices = "devices";
+let rod4= "Drey";
+let rs = "responsive sea";
+let guide= "guide";
+let adapting= "Adapting";
+let fluidity= "fluidity";
+
+let sentence25 = across + " screens, " + devices + ', ' + "and dimensions, ";
+let sentence26 = rod + "sails on the " + rs;
+let sentence27 = "Media queries " + guide + "his intentions, ";
+let sentence28 = adapting + "layouts with " + fluidity;
+
+//poem7
+let poem7= "The SEO Whisperer";
+
+let labyrinth= "labyrinth";
+let rod5 = "Drey";
+let seo = "SEO whisperer";
+let mt = "Meta tags";
+let wanderers = "wanderers";
+
+let sentence29 = "In the " + labyrinth + " of search engines,";
+let sentence30 = rod + " becomes an " + seo;
+let sentence31 = mt + ', ' + "keywords, backlink strings, ";
+let sentence32 = "He crafts pathways to digital " + wanderers;
+
+//poem8
+let poem8 = "The Git Alchemist";
+
+let mp= "mystical potion";
+let rod6= "Drey";
+let Gits= "Gits";
+let merge = "merge";
+let rep= "repository";
+let code1= "code";
+
+let sentence33 = "Version control, a" + mp;
+let sentence34 = rod + "brews commits in " + Gits + "cauldron,";
+let sentence35 = "Branches fork, " + merge + "conflicts in motion,";
+let sentence36 = "His " + rep + "a saga of " + code + "reborn." ;
+
+//poem9
+let poem9 = "The Deployment Dreamer";
+
+let moon = "moon";
+let rod7= "Drey";
+let deployment = "Deployments";
+let web = "website";
+let unfurled = "unfurled";
+let wrld = "world,";
+
+let sentence37 = "And when the " + moon + "kisses the horizon, ";
+let sentence38 = rod + "unfurls his creation to the " + wrld;
+let sentence39 = deployment + "like shooting stars, arisen, ";
+let sentence40 = "His " + web + "sails, its sails " + unfurled;
+
+
+//poem10
+let poem10 = "The Web Developer's Anthem";
+
+let wd = "web developer";
+let pixels1 = "pixels";
+let binary = "binary";
+let matrix = "matrix";
+let cl = "click";
+
+
+let sentence41 = "So here's to " + rod + ', ' + "the " + wd;
+let sentence42 = "A poet of " + pixels + ', ' + "a composer " + cl;
+let sentence43 = "In the " + binary + "symphony, he's the reveler";
+let sentence44 = "Waving of dreams into the digital" + matrix + '. ';
+
+let poemDiv = document.getElementById("poem");
+
+
+poem.innerHTML += "<p>" + poem0 + "</p>";
+poem.innerHTML += "<p>" + sentence1 + "</p>";
+poem.innerHTML += "<p>" + sentence2 + "</p>";
+poem.innerHTML += "<p>" + sentence3 + "</p>";
+poem.innerHTML += "<p>" + sentence4 + "</p>";
+
+//01
+poem.innerHTML += "<p>" + poem1 + "</p>";
+poem.innerHTML += "<p>" + sentence5 + "</p>";
+poem.innerHTML += "<p>" + sentence6 + "</p>";
+poem.innerHTML += "<p>" + sentence7 + "</p>";
+poem.innerHTML += "<p>" + sentence8 + "</p>";
+//02
+poem.innerHTML += "<p>" + poem2 + "</p>";
+poem.innerHTML += "<p>" + sentence9 + "</p>";
+poem.innerHTML += "<p>" + sentence10 + "</p>";
+poem.innerHTML += "<p>" + sentence11 + "</p>";
+poem.innerHTML += "<p>" + sentence12 + "</p>";
+//03
+poem.innerHTML += "<p>" + poem3 + "</p>";
+poem.innerHTML += "<p>" + sentence13 + "</p>";
+poem.innerHTML += "<p>" + sentence14 + "</p>";
+poem.innerHTML += "<p>" + sentence15 + "</p>";
+poem.innerHTML += "<p>" + sentence16 + "</p>";
+//04
+poem.innerHTML += "<p>" + poem4 + "</p>";
+poem.innerHTML += "<p>" + sentence17 + "</p>";
+poem.innerHTML += "<p>" + sentence18 + "</p>";
+poem.innerHTML += "<p>" + sentence19 + "</p>";
+poem.innerHTML += "<p>" + sentence20 + "</p>";
+//05
+poem.innerHTML += "<p>" + poem5 + "</p>";
+poem.innerHTML += "<p>" + sentence21 + "</p>";
+poem.innerHTML += "<p>" + sentence22 + "</p>";
+poem.innerHTML += "<p>" + sentence23 + "</p>";
+poem.innerHTML += "<p>" + sentence24 + "</p>";
+//06
+poem.innerHTML += "<p>" + poem6 + "</p>";
+poem.innerHTML += "<p>" + sentence25 + "</p>";
+poem.innerHTML += "<p>" + sentence26 + "</p>";
+poem.innerHTML += "<p>" + sentence27 + "</p>";
+poem.innerHTML += "<p>" + sentence28 + "</p>";
+//07
+poem.innerHTML += "<p>" + poem7 + "</p>";
+poem.innerHTML += "<p>" + sentence29 + "</p>";
+poem.innerHTML += "<p>" + sentence30 + "</p>";
+poem.innerHTML += "<p>" + sentence31 + "</p>";
+poem.innerHTML += "<p>" + sentence32 + "</p>";
+//08
+poem.innerHTML += "<p>" + poem8 + "</p>";
+poem.innerHTML += "<p>" + sentence33 + "</p>";
+poem.innerHTML += "<p>" + sentence34 + "</p>";
+poem.innerHTML += "<p>" + sentence35 + "</p>";
+poem.innerHTML += "<p>" + sentence36 + "</p>";
+//09
+poem.innerHTML += "<p>" + poem9 + "</p>";
+poem.innerHTML += "<p>" + sentence37 + "</p>";
+poem.innerHTML += "<p>" + sentence38 + "</p>";
+poem.innerHTML += "<p>" + sentence39 + "</p>";
+poem.innerHTML += "<p>" + sentence40 + "</p>";
+//10
+poem.innerHTML += "<p>" + poem10 + "</p>";
+poem.innerHTML += "<p>" + sentence41 + "</p>";
+poem.innerHTML += "<p>" + sentence42 + "</p>";
+poem.innerHTML += "<p>" + sentence43 + "</p>";
+poem.innerHTML += "<p>" + sentence44 + "</p>";
+
